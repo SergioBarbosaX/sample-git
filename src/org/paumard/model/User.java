70355b74ec0163429d14dce1f62144450b9c0ff4
Sergio Barbosa <serbarbos@hotmail.com>
2020-10-15 21:30:47 +0200

    Added constructors

--- src/org/paumard/model/User.java
+++ src/org/paumard/model/User.java
@@ -4,6 +4,16 @@ public class User {
 
 	private String name;
 	private int age;
+	
+	public User() {
+		super();
+	}
+
+	public User(String name, int age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
 
 	public int getAge() {
 		return age;
@@ -25,5 +35,7 @@ public class User {
 	public String toString() {
 		return "User [name=" + name + ", age=" + age + "]";
 	}
+
+	
 	
 }
